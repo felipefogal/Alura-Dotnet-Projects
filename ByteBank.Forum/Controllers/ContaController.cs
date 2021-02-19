537a7f5f4487a29138dfb83590e00771e85de5f5
Felipe Garcia Fogal <felipe.fogal@gruposbf.com.br>
2021-02-19 16:56:51 -0300

    Subindo conteúdo do projeto de aprendizado de Identity da Alura

---
+++ ByteBank.Forum/Controllers/ContaController.cs
@@ -0,0 +1,111 @@
+﻿using ByteBank.Forum.Models;
+using ByteBank.Forum.ViewModels;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ByteBank.Forum.Controllers
+{
+    public class ContaController : Controller
+    {
+        public ActionResult Registrar()
+        {
+            return View();
+        }
+
+        private UserManager<UsuarioAplicacao> _userManager;
+        public UserManager<UsuarioAplicacao> UserManager
+        {
+            get
+            {
+                if(_userManager == null)
+                {
+                    var contextOwin = HttpContext.GetOwinContext();
+                    _userManager = contextOwin.GetUserManager<UserManager<UsuarioAplicacao>>();
+                }
+                return _userManager;
+            }
+            set
+            {
+                _userManager = value;
+            }
+        }
+        
+
+        [HttpPost]
+        public async Task<ActionResult> Registrar(ContaRegistrarViewModel modelo)
+        {
+            if (ModelState.IsValid)
+            {
+                var novoUsuario = new UsuarioAplicacao();
+
+                novoUsuario.Email = modelo.Email;
+                novoUsuario.UserName = modelo.UserName;
+                novoUsuario.NomeCompleto = modelo.NomeCompleto;
+
+                var usuario = await UserManager.FindByEmailAsync(modelo.Email);
+                var usuarioJaExiste = usuario!= null;
+
+                if (usuarioJaExiste)
+                    return RedirectToAction("AguardandoConfirmacao");
+
+
+                var resultado = await UserManager.CreateAsync(novoUsuario, modelo.Senha);
+
+                if (resultado.Succeeded)
+                {
+                    // Enviar o email de confirmação
+                    await EnviarEmailDeConfirmacaoAsync(novoUsuario);
+                    return View("AguardandoConfirmacao");
+                }
+                else
+                {
+                    AdicionaErros(resultado);
+                }
+            }
+
+            // erro
+            return View(modelo);
+        }
+
+        private async Task EnviarEmailDeConfirmacaoAsync(UsuarioAplicacao usuario)
+        {
+            var token = await UserManager.GenerateEmailConfirmationTokenAsync(usuario.Id);
+            var linkDeCallback = Url.Action("ConfirmacaoEmail",
+                "Conta",
+                new { usuarioId = usuario.Id, token = token },
+                Request.Url.Scheme);
+
+            await UserManager.SendEmailAsync(usuario.Id, 
+                "Fórum ByteBank - Confirmação de Email", 
+                $"Bem vindo ao Fórum Byte Bank, use o código {linkDeCallback} para confirmar seu endereço de email!");
+        }
+
+        public async Task<ActionResult> ConfirmacaoEmail(string usuarioId, string token)
+        {
+            if (usuarioId == null || token == null)
+                return View("Error");
+
+            var resultado = await UserManager.ConfirmEmailAsync(usuarioId, token);
+
+            if (resultado.Succeeded)
+                return RedirectToAction("Index", "Home");
+            else
+                return View("Error");
+        }
+
+        private void AdicionaErros(IdentityResult resultado)
+        {
+            foreach (var erro in resultado.Errors)
+            {
+                ModelState.AddModelError("", erro);
+            }
+        }
+    }
+}
